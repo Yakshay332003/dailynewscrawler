24bc71a12d85553aa4ad6467815cb054cde9d5b7
Yakshay332003 <125949271+Yakshay332003@users.noreply.github.com>
2025-09-05 22:59:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -205,7 +205,7 @@ def extract_article_text(url):
 # --- Streamlit App
 # -------------------------------
 st.set_page_config(page_title="📰 Hunt News", layout="wide")
-st.title("📰 Keyword News Explorer ")
+st.title("📰 Hunt News by keyword or company name ")
 
 # Input Section
 with st.form("fetch_form"):
